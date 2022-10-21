ef5564140d957886f16c506011e260ac474f4c5c
codeposse <codeposse@gmail.com>
2022-10-21 15:10:46 -0700

    NPM

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,9 @@
 var gulp = require("gulp");
-var cheerio = require('gulp-cheerio') //use jquery in gulp
-var gutil = require('gulp-util'); //utilities
 var plumber = require('gulp-plumber'); //error handler
 var prettify = require('gulp-prettify'); //properly formats HTML
 var uglify = require('gulp-uglify');
-var uglifycss = require('gulp-uglifycss');
 var htmlmin = require('gulp-htmlmin'); //html uglify
 
-gulp.task("default", async function() {
-    gutil.log(gutil.colors.bgGreen.white.bold('GULP WORKS'), gutil.colors.bgRed.white.bold("type: \"gulp --tasks\" to list all tasks"));
-});
 
 gulp.task('miniHTML', async function() {
     gulp.src('./jury/*.html') //DUDE, CHANGE ME
