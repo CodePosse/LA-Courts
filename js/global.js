a501a4f62bc85a191df1ed0b1977fd61e2608c0b
T.Hunold <codeposse@gmail.com>
2023-10-23 15:43:06 -0700

    Domain: location.hostname,

--- js/global.js
+++ js/global.js
@@ -6,3 +6,13 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 //toasts
 $(".toast").show();
 $(".toast button").click(function () { $(this).closest(".toast").hide() });
+var title = "codeposse";
+    a = {
+      Author: "T. Hunold",
+      Github: title,
+      Location: "LA County Court",
+      Site_Professional: "linkedin.com/in/itssobig",
+
+    };
+    console.info("%c T. Hunold github: codeposse", "background: #222; color: #bada55");
+    console.dir(a);
