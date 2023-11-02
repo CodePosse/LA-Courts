1606d298dd2de25746b4b666236aa4f37f1011db
T.Hunold <codeposse@gmail.com>
2023-11-02 09:25:59 -0700

    step 4 statuses

--- jury/js/global.js
+++ jury/js/global.js
@@ -1,23 +1,23 @@
 //tooltips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
+  return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 //toasts
 $(".toast").show();
 $(".toast button").click(function () { $(this).closest(".toast").hide() });
 var title = "codeposse";
-    a = {
-      Author: "T. Hunold",
-      Github: title,
-      Location: "LA County Court",
-      Site_Professional: "linkedin.com/in/itssobig",
-      Site: location.hostname
-    };
-    console.info("%c Author: T. Hunold; Git: codeposse; " + location.hostname, "background: #222; color: #bada55");
-    console.dir(a);
+a = {
+  Author: "T. Hunold",
+  Github: title,
+  Location: "LA County Court",
+  Site_Professional: "linkedin.com/in/itssobig",
+  Site: location.hostname
+};
+console.info("%c Author: T. Hunold; Git: codeposse; " + location.hostname, "background: #222; color: #bada55");
+console.dir(a);
 //img missing
-$( document ).ready(function() {
+$(document).ready(function () {
   $("img").each(function () {
     if (
       (typeof this.naturalWidth != "undefined" &&
