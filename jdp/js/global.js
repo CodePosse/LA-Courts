90ba382618247b34f669ac14696c2c01d348a354
T.Hunold <codeposse@gmail.com>
2024-01-30 09:00:47 -0800

    global

--- jdp/js/global.js
+++ jdp/js/global.js
@@ -13,24 +13,7 @@ a = {
   Author: "T. Hunold",
   Github: title,
   Location: "LA County Court",
-  Site_Professional: "linkedin.com/in/itssobig",
+  LinkedIn: "linkedin.com/in/itssobig",
   Site: location.hostname
 };
-console.info("%c Author: T. Hunold; Git: codeposse; " + location.hostname, "background: #222; color: #bada55");
 console.dir(a);
-//img missing
-$(document).ready(function () {
-  $("img").each(function () {
-    if (
-      (typeof this.naturalWidth != "undefined" &&
-        this.naturalWidth == 0) ||
-      this.readyState == "uninitialized"
-    ) {
-      $(this).attr({
-        src: "./img/img-placeholder.webp",
-        alt: "broken"
-      });
-    }
-  });
-  console.log('img 404 check complete')
-});
