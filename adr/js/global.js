8dc40687e4ada1bfc5c2370dfe6a756d637add52
T.Hunold <codeposse@gmail.com>
2024-02-01 10:12:46 -0800

    global.js

---
+++ adr/js/global.js
@@ -0,0 +1,22 @@
+//Globals
+
+
+//tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
+//dismissable callouts
+$(".callout-hide").click(function () { $(this).closest(".bd-callout").hide() });
+//toasts
+$(".toast").show();
+$(".toast button").click(function () { $(this).closest(".toast").hide() });
+var title = "codeposse";
+a = {
+  Author: "T. Hunold",
+  Github: title,
+  Location: "LA County Court",
+  LinkedIn: "linkedin.com/in/itssobig",
+  Site: location.hostname
+};
+console.dir(a);
