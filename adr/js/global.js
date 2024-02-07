ec2096b4318d009b53ea9bbc77b15c6b1092059b
T.Hunold <codeposse@gmail.com>
2024-02-07 12:33:56 -0800

    filters

--- adr/js/global.js
+++ adr/js/global.js
@@ -20,3 +20,35 @@ a = {
   Site: location.hostname
 };
 console.dir(a);
+
+
+//filters
+$(function () {
+  $("a.All").click(function () {
+    $(".card").show();
+  });
+  $("a.showCH").click(function () {
+    $(".card:not(.ch)").hide();
+    $(".ch").show();
+  });
+  $("a.showSC").click(function () {
+    $(".card:not(.sc)").hide();
+    $(".sc").show();
+  });
+  $("a.showLC").click(function () {
+    $(".card:not(.lc)").hide();
+    $(".lc").show();
+  });
+  $("a.showUD").click(function () {
+    $(".card:not(.ud)").hide();
+    $(".ud").show();
+  });
+  $("a.showFL").click(function () {
+    $(".card:not(.fl)").hide();
+    $(".fl").show();
+  });
+  $("a.showPROB").click(function () {
+    $(".card:not(.prob)").hide();
+    $(".prob").show();
+  });
+});
