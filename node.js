0b2dfad9dd615115901fbaa646809e014f3707d1
T.Hunold <codeposse@gmail.com>
2024-06-28 17:32:34 -0700

    ada

--- node.js
+++
@@ -1,5 +0,0 @@
-var minify = require('html-minifier').minify;
-var result = minify('<p title="blah" id="moo">foo</p>', {
-  removeAttributeQuotes: true
-});
-result; // '<p title=blah id=moo>foo</p>'
