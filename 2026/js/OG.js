d2ee5a8d68d2191908170484f2f00309f3a33464
T.Hunold <codeposse@gmail.com>
2026-03-05 11:37:26 -0800

    :wrench: opengraph additions

--- 2026/js/OG.js
+++ 2026/js/OG.js
@@ -2,8 +2,11 @@
 $(window).on("load", function () {
     var current_title = $(document).attr('title');
     $('head').append('<meta property="og:title" content="' + current_title + '" />');
+    $('head').append('<meta property="og:description" content="' + current_title + '" />');
     $('head').append('<meta property="og:type" content="website" />');
+    $('head').append('<meta property="og:url" content="' + window.location.href + '" />');
     // Twitter Card Tags (for Social & Accessibility)
     $('head').append('<meta name="twitter:title" content="' + current_title + '" />');
     $('head').append('<meta name="twitter:description" content="' + current_title + '" />');
+    $('head').append('<meta name="twitter:card" value="summary_large_image" />');
 });
